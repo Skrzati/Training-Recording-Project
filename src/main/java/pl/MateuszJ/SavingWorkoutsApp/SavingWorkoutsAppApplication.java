99f02cb14d58c9e77c981322676d3662d54ddc17
Skrzati <mati.janusz6@gmail.com>
2025-12-02 23:30:17 +0100

    Start Of Project

---
+++ src/main/java/pl/MateuszJ/SavingWorkoutsApp/SavingWorkoutsAppApplication.java
@@ -0,0 +1,13 @@
+package pl.MateuszJ.SavingWorkoutsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingWorkoutsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingWorkoutsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/MateuszJ/SavingWorkoutsApp/SavingWorkoutsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.MateuszJ.SavingWorkoutsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingWorkoutsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
