e2c82e0f28bdceaf56e82ccbef3e1a40c61eb89c
Skrzati <mati.janusz6@gmail.com>
2025-12-09 23:23:08 +0100

    Create User,UserService,UserRepository,UserController

---
+++ src/main/java/pl/MateuszJ/SavingWorkoutsApp/controller/UserController.java
@@ -0,0 +1,29 @@
+package pl.MateuszJ.SavingWorkoutsApp.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.MateuszJ.SavingWorkoutsApp.model.User;
+import pl.MateuszJ.SavingWorkoutsApp.services.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.setUsers(user);
+    }
+}
--- src/main/java/pl/MateuszJ/SavingWorkoutsApp/repository/UserRepository.java
+++ src/main/java/pl/MateuszJ/SavingWorkoutsApp/repository/UserRepository.java
@@ -8,6 +8,4 @@ public interface UserRepository extends JpaRepository<User , Integer> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
 }
