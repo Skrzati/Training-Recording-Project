fca7e9eadf2a7d1f3e444e53bc3243529f966e9f
Skrzati <mati.janusz6@gmail.com>
2025-12-09 23:03:35 +0100

    Create a Package

---
+++ src/main/java/pl/MateuszJ/SavingWorkoutsApp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package pl.MateuszJ.SavingWorkoutsApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.MateuszJ.SavingWorkoutsApp.model.User;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User , Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
---
+++ src/main/java/pl/MateuszJ/SavingWorkoutsApp/services/UserService.java
@@ -0,0 +1,8 @@
+package pl.MateuszJ.SavingWorkoutsApp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
